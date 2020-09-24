b11df1bbee4f05c5683c1ded848f610c866f18ce
김호정 <hjnk125@gmail.com>
2020-09-24 16:12:44 +0900

    [ADD]Signin을 구현하였습니다.

---
+++ client/src/component/FakeData_Signin.js
@@ -0,0 +1,29 @@
+export const fakeData = [
+    {
+        "id": 1,
+        "nickname": "a",
+        "password": "a",
+        "email": "a@a.com",
+        "updatedAt": "created time",
+        "createdAt": "updated time",
+        "photo": "photo"
+    },
+    {
+        "id": 2,
+        "nickname": "b",
+        "password": "b",
+        "email": "b@b.com",
+        "updatedAt": "created time",
+        "createdAt": "updated time",
+        "photo": "photo"
+    },
+    {
+        "id": 3,
+        "nickname": "c",
+        "password": "c",
+        "email": "c@c.com",
+        "updatedAt": "created time",
+        "createdAt": "updated time",
+        "photo": "photo"
+    }
+]
